e245aa0308f6d26505a828eed74add2cb2ff48f6
Jaedan Persaud <jaedanpersaud12@gmail.com>
2024-09-10 00:33:28 -0400

    chore: use shadcn button for login

--- src/app/_components/top-nav.tsx
+++ src/app/_components/top-nav.tsx
@@ -1,14 +1,17 @@
+import { Button } from "@/components/ui/button";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 function TopNav() {
   return (
-    <div className="flex items-center justify-between border-b bg-gray-800 p-4">
-      <div className="text-white">
+    <div className="flex items-center justify-between border-b p-4">
+      <div className="">
         <h1 className="text-2xl font-bold">Gallery</h1>
       </div>
       <div className="flex items-center gap-4">
         <SignedOut>
-          <SignInButton />
+          <Button variant="outline">
+            <SignInButton />
+          </Button>
         </SignedOut>
         <SignedIn>
           <UserButton />
