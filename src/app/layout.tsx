b82e191f81ffe0e1091a69e2eebd8dd5da046625
Jaedan Persaud <jaedanpersaud12@gmail.com>
2024-09-09 22:43:30 -0400

    chore: add intial ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,12 +9,30 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
+function TopNav() {
+  return (
+    <div className="flex items-center justify-between border-b bg-gray-800 p-4">
+      <div className="text-white">
+        <h1 className="text-2xl font-bold">Gallery</h1>
+      </div>
+      <div className="flex items-center gap-4">
+        <button className="rounded-md bg-gray-700 px-4 py-2 text-white">
+          Sign In
+        </button>
+      </div>
+    </div>
+  );
+}
+
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
-      <body>{children}</body>
+      <body className="flex flex-col gap-4">
+        <TopNav />
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
-import Link from "next/link";
+import Image from "next/image";
+
+const mockUrls = [
+  "https://utfs.io/f/901c36d6-45f3-4ac1-b14d-0282ca8736d6-mqi9le.jpg",
+  "https://utfs.io/f/0254134d-6b56-44cc-b21f-e512c420ae72-k9mxhx.jpg",
+];
+
+const mockImages = mockUrls.map((url, index) => {
+  return {
+    id: index + 1,
+    url,
+  };
+});
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello swagga lord
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id}>
+            <Image src={image.url} alt="mock" width={200} height={200} />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
