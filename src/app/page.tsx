09beeb0c0a75f3e529e6e68a2aefc2cfa67c7b90
Jaedan Persaud <jaedanpersaud12@gmail.com>
2024-09-10 00:42:18 -0400

    chore: use auth state to display dynamic info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,40 @@
 import { db } from "@/server/db";
 import Image from "next/image";
-
+import { images } from "@/server/db/schema";
+import { SignedOut, SignedIn } from "@clerk/nextjs";
 export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
+  return (
+    <main className="mx-10">
+      <SignedOut>
+        <div>Please sign in to view the images</div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
+    </main>
+  );
+}
+
+const Images = async () => {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
 
-  console.log(images);
-
   return (
-    <main className="mx-10">
+    <div className="flex flex-wrap gap-4">
       {images.map((image) => (
-        <div key={image.id}>
-          <div className="text-2xl font-bold text-white">{image.name}</div>
-          <Image src={image.url} alt="mock" width={200} height={200} />
-        </div>
+        <ImageCard key={image.id} image={image} />
       ))}
-    </main>
+    </div>
   );
-}
+};
+
+const ImageCard = async ({ image }: { image: typeof images.$inferSelect }) => {
+  return (
+    <div>
+      <Image src={image.url} alt={image.name} width={200} height={200} />
+    </div>
+  );
+};
