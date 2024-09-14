41e5ea017df79adf25bd0b75e0585befab83ef46
Jaedan Persaud <jaedanpersaud12@gmail.com>
2024-09-13 22:57:50 -0400

    chore: add user button to dashboard header

--- src/app/dashboard/_components/header/header.tsx
+++ src/app/dashboard/_components/header/header.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import { MobileNavToggle, SearchBar, UserMenu } from "./components";
+import { UserButton } from "@clerk/nextjs";
+import { MobileNavToggle, SearchBar } from "./components";
 
 export const Header = () => {
   return (
     <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
       <MobileNavToggle />
       <SearchBar />
-      <UserMenu />
+      <UserButton />
     </header>
   );
 };
